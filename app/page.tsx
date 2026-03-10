949138b8ece0460e6a31031b0930262be2fed355
Suraj-Shendge <124484652+Suraj-Shendge@users.noreply.github.com>
2026-03-11 00:43:52 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,13 @@
+import ProblemList from "@/components/ProblemList";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 style={{ marginBottom: "1rem", fontSize: "2rem" }}>
+        Practice Problems
+      </h1>
+      <ProblemList />
+    </main>
+  );
+}
 
